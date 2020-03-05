9573bf4005cb283f5bf70b1fe48247d37725a4d4
Diana Karimova <46958821+DianaKarim@users.noreply.github.com>
2020-03-05 13:04:24 +0100

    changes in the main example code

--- network_example.R
+++ network_example.R
@@ -1,10 +1,15 @@
+library(truncnorm)
+library(extraDistr)
+library(statmod)
+library(mvtnorm)
 
 library(parallel)
 library(reshape2)
+library(ggplot2)
 
 # Data generation:
 
-N <- 16 # number of actors
+N <- 15 # number of actors
 P <- 1 # enumber of covariates 
 D <- 2 # number of messages for every potential receiver in most extreme case (maybe use more (less) depending on result)
 M <- (N-1) * D #number of events 
@@ -22,51 +27,15 @@ Sigma <- create.sigma(2.5, 0.5, d = N)
 R1 <- rep(2,M) #one extreme where actor 2 is always receiver
 R2 <- rep(2:N,each=D) #other extreme where receiver is exactly as expected by predictor variable X
 #actual receivers go from extreme R1 to other extreme R2
+
 step <- 7 #step can be D (for R1) ... N (for R2)
 R <- c(R2[1:step],rep(2,M-step))
-R
+#R
 
 #Estimation
 
 Nsample_test <- 10000
 burn_in_test <- 2000
-
-lasso_res <- unlist(lapply(D:M, function(step){
-  R <- c(R2[1:step],rep(2,M-step))
-  res <- lasso(S, R, X, Sigma, Nsample = Nsample_test, burn_in = burn_in_test)
-  mean(res$Beta)
-}))
-
-#plot(lasso_res, type = "l")
-
-hs_res <- unlist(lapply(D:M, function(step){
-  R <- c(R2[1:step],rep(2,M-step))
-  res <- horseshoe(S, R, X, Sigma, Nsample = Nsample_test, burn_in = burn_in_test)
-  mean(res$Beta)
-}))
-
-#plot(hs_res, type = "l", col = "red")
-
-ridge_res <- unlist(lapply(D:M, function(step){
-  R <- c(R2[1:step],rep(2,M-step))
-  res <- ridge(S, R, X, Sigma, Nsample = Nsample_test, burn_in = burn_in_test)
-  mean(res$Beta)
-}))
-
-plot(ridge_res, type = "l", col = "blue")
-
-
-# Improper
-imp_res <- unlist(lapply(D:M, function(step){
-  R <- c(R2[1:step],rep(2,M-step))
-  res <- imp(S, R, X, Sigma, Nsample = Nsample_test, burn_in = burn_in_test)
-  mean(res$Beta)
-}))
-
-#plot(imp_res, type = "l", col = "blue")
-
-
-
 
 #########################################################################################################
 #### All 4 models in parallel ----
@@ -74,7 +43,7 @@ imp_res <- unlist(lapply(D:M, function(step){
 
 
 cl<-makeCluster(3)
-clusterExport(cl, c("R2", "S", "R", "X", "D", "M", "imp", "lasso", "ridge", "horseshoe", "Nsample_test", 
+clusterExport(cl, c("R2", "S", "R", "X", "D", "M", "imp", "lasso.new", "ridge", "horseshoe", "Nsample_test", 
                     "burn_in_test", "Sigma", "generate.latent.lapply"))
 clusterEvalQ(cl, {library(truncnorm)
   library(extraDistr)
@@ -96,7 +65,7 @@ ridge_res <-unlist(parLapply(cl, D:M, function(step){
 
 lasso_res <-unlist(parLapply(cl, D:M, function(step){
   R <- c(R2[1:step],rep(2,M-step))
-  res <- lasso(S, R, X, Sigma, Nsample = Nsample_test, burn_in = burn_in_test)
+  res <- lasso.new(S, R, X, Sigma, Nsample = Nsample_test, burn_in = burn_in_test)
   mean(res$Beta)
 } ))
 
@@ -109,29 +78,9 @@ hs_res <-unlist(parLapply(cl, D:M, function(step){
 
 stopCluster(cl)
 
+# Save the results ----
 
-# Add lines to save the results 
-
-
-### Estimate only hs -----
-
-cl<-makeCluster(3)
-clusterExport(cl, c("R2", "S", "R", "X", "D", "M", "imp", "lasso", "ridge", "horseshoe", "Nsample_test", 
-                    "burn_in_test", "Sigma", "generate.latent.lapply"))
-clusterEvalQ(cl, {library(truncnorm)
-  library(extraDistr)
-  library(statmod)
-  library(mvtnorm)})
-
-hs_res <-unlist(parLapply(cl, D:M, function(step){
-  R <- c(R2[1:step],rep(2,M-step))
-  res <- horseshoe(S, R, X, Sigma, Nsample = Nsample_test, burn_in = burn_in_test)
-  mean(res$Beta)
-} ))
-
-stopCluster(cl)
-
-
+save(list = c("imp_res", "ridge_res", "lasso_res", "hs_res"), file = "results_simple_exmpl_newlasso.Rdata")
 
 
 # Vizz ----
@@ -140,6 +89,9 @@ plot(imp_res, type = "l")
 lines(lasso_res, type = "l", col = "green")
 lines(hs_res, col = "red")
 lines(ridge_res, type = "l", col = "blue")
+
+plot(lasso_res, type = "l", col = "green")
+
 
 plot(imp_res, lasso_res, type = "l", col = "green")
 lines(imp_res, ridge_res, type = "l", col = "blue")
@@ -154,13 +106,13 @@ estim_all_dt  <-  melt(estim_all, id=c("index"))
 
 #final plot with all estimates:
 ggplot(estim_all_dt) + geom_line(aes(x=index, y=value, colour=variable)) +
-  scale_colour_manual(values=c("black","red","blue", "green")) +
+  scale_colour_manual(values=c("black","blue","green", "red")) +
   labs( x = "Dataset Index", y = "Estimates")
 
 # All estimates vs improper 
 df <- melt(estim_all[,-1], id = c("improper"))
 ggplot(df) + geom_line(aes(x=improper, y=value, colour=variable)) +
-  scale_colour_manual(values=c("red","blue", "green")) +
+  scale_colour_manual(values=c("blue","green", "red")) +
   labs(x = "Improper prior estimates", y = "Estimates")
 
 
